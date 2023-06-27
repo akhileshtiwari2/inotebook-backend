d023cb72c73b6a4a478d992f2b24c7f91b8bc5c4
akhileshtiwari2 <akhilesh.tiwari02@gmail.com>
2023-06-27 21:00:15 +0530

    Adding Data Validation Using express-validator Code Refactoring

--- routes/auth.js
+++ routes/auth.js
@@ -5,28 +5,38 @@ const { body, validationResult } = require("express-validator");
 
 //Create aUser using: POST "/api/auth/". Doesn't require Auth
 router.post(
-  "/",
+  "/createuser",
   [
     body("name", "Enter a Valid Name").isLength({ min: 3 }),
     body("email", "Enter a Valid Email").isEmail(),
     body("password", "Enter a Valid Password").isLength({ min: 5 }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    //If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-
-    User.create({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-        res.json({ error: "Please enter a unique value for email" });
+    //Check whether the user with this email exists already
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      }
+      // Create a New User
+      user = await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
       });
+      res.json(user);
+      //Catch errors
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some Error occurred ");
+    }
   }
 );
 module.exports = router;
