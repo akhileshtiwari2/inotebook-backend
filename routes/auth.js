dc7d5bd8a4dbe0aa8ea312eee6cdcb91c1471036
akhileshtiwari2 <akhilesh.tiwari02@gmail.com>
2023-06-29 11:09:46 +0530

    Fetching all notes & Adding a Note

--- routes/auth.js
+++ routes/auth.js
@@ -62,24 +62,27 @@ router.post(
   [
     body("email", "Enter a Valid Email").isEmail(),
     body("password", "Password cannot be blank").exists(),
-
   ],
   async (req, res) => {
-     //If there are errors, return Bad request and the errors
-     const errors = validationResult(req);
-     if (!errors.isEmpty()) {
-       return res.status(400).json({ errors: errors.array() });
-     }
+    //If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-     const {email,password} = req.body;
-     try {
-      let user = await User.findOne({email});
-      if(!user){
-        return res.status(400).json({error:"Please try to login correct credentials"});
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login correct credentials" });
       }
-      const passwordCompare = await bcrypt.compare(password,user.password);
-      if(!passwordCompare){
-        return res.status(400).json({error:"Please try to login correct credentials"});
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login correct credentials" });
       }
       const data = {
         user: {
@@ -88,24 +91,23 @@ router.post(
       };
       const authtoken = jwt.sign(data, JWT_SECRET);
       res.json({ authtoken });
-     } 
-     catch (error) {
+    } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal server error");
     }
-  })
+  }
+);
 //ROUTE 3: Get loggedIn User Details using: POST "/api/auth/getuser". login required
 
-router.post('/getuser', fetchuser, async(req,res)=>{
-  try{
+router.post("/getuser", fetchuser, async (req, res) => {
+  try {
     userId = req.user.id;
-    const user = await User.findById(userId).select("-password")
-    res.send(user)
-  }catch (error) {
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
     console.error(error.message);
     res.status(500).send("Internal server error");
   }
-
-})
+});
 
 module.exports = router;
--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,47 @@
 const express = require("express");
 const router = express.Router();
+const fetchuser = require("../middleware/fetchuser");
+const Note = require("../models/Note");
+const { body, validationResult } = require("express-validator");
 
-router.get("/", (req, res) => {
-  res.json([]);
+//ROUTE 1: Get All the Notes using: GET "/api/auth/fetchallnotes".
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  const notes = await Note.find({ user: req.user.id });
+  res.json(notes);
 });
+
+//ROUTE 1: Get All the Notes using: GET "/api/auth/addnote".
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a Valid Name").isLength({ min: 3 }),
+    body("description", "Description must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      //If there are errors, return Bad request and the errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const savedNote = await note.save();
+
+      res.json(savedNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some Error occurred ");
+    }
+  }
+);
 
 module.exports = router;
